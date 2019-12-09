3740c5ebb76ac1312f4b52176a726c7d35795004
zengqingzhou@123.com <zengqingzhou@outlook.com>
2019-12-09 12:52:28 +0800

    xinshe

--- api-impl/src/main/java/com/xinshe/web/impl/controller/UserController.java
+++ api-impl/src/main/java/com/xinshe/web/impl/controller/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
         if ("success".equals(result)){
             return ResultUtil.ok();
         }
-        return ResultUtil.error("账户或者密码错误!");
+        return ResultUtil.error("账户或者密码错误! ");
     }
 
     @RequestMapping(value = "/toLogin", method = RequestMethod.GET)
