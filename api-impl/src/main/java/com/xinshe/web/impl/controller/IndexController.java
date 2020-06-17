b0d3292e1e76e3320cc3eae4ee5b5d315b18de50
zengqingzhou <zengqingzhou@cdvcloud.com>
2020-06-17 15:04:12 +0800

    tag

--- api-impl/src/main/java/com/xinshe/web/impl/controller/IndexController.java
+++ api-impl/src/main/java/com/xinshe/web/impl/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class IndexController {
+    //tag
     @RequestMapping(value = "/" ,method = RequestMethod.GET)
     public String index(){
         return "/index";
